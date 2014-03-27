0da3d80b247b678e9dbc8f5d0769ff3bfb3f8105
Jordan West <jordanrw@gmail.com>
2014-03-27 13:22:15 -0700

    extend list-issues to support grouping by label, in addition to milestone

--- issues/__init__.py
+++ issues/__init__.py
@@ -9,3 +9,6 @@ def milestone_by_name(repo, milestone_name):
 
 def open_issues_in_milestone(repo, milestone):
     return repo.get_issues(milestone=milestone, state='open')
+
+def open_issues_by_label(repo, labels):
+    return repo.get_issues(state='open', labels = labels)
